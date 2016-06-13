13368f964c1864d8b286ccd2cb3b1589235314c4
vinicius <vinicius@contabilizei.com.br>
2016-06-13 10:19:50 -0300

    q!

--- src/main/java/br/com/buscacep/controller/BuscaCepController.java
+++ src/main/java/br/com/buscacep/controller/BuscaCepController.java
@@ -59,10 +59,8 @@ public class BuscaCepController {
 	/**
 	 * Busca pelo Cep.
 	 * 
-	 * @param num
+	 * @param numCep
 	 *            Dados do CEP.
-	 * @throws NenhumCepEncontradoException
-	 * @throws CepInvalidoException
 	 */
 	@RequestMapping(value = "/buscacep/buscaCep/{numCep}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Busca o cep na base de dados", notes = "Busca um cep na base de dados.")
